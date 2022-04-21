8b75a11d06201bbab1548df02aaed534f71ca567
Alexandra Mikhailova <morgen270@live.com>
2022-04-21 14:10:43 +0300

    Add project files.

---
+++ DRGPetShop/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using DRGPetShop.MVC.Data;
+using DRGPetShop.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DRGPetShop.MVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly DrgContext _context;
+        public CategoryController(DrgContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categoryList = _context.Category;
+            return View(categoryList);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category item)
+        {
+            _context.Category.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ DRGPetShop/Data/DrgContext.cs
@@ -0,0 +1,15 @@
+﻿using DRGPetShop.MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DRGPetShop.MVC.Data
+{
+    public class DrgContext : DbContext
+    {
+        public DrgContext(DbContextOptions<DrgContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
